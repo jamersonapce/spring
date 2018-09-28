bb849f5ca584659edebb3e2705f5012041c2e968
Jamerson <jamerson.silva@acad.ifma.edu.br>
2018-09-28 15:18:00 -0300

    Curso Spring 3.5

---
+++ src/main/java/com/jamerson/brewer/config/init/AppInitializer.java
@@ -0,0 +1,24 @@
+package com.jamerson.brewer.config.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.jamerson.brewer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
