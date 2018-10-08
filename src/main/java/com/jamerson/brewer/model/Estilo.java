dcbf90762486908a03e3c47c2c8177546d21b6fc
Jamerson <jamerson.silva@acad.ifma.edu.br>
2018-10-08 18:56:29 -0300

    Curso Spring 8.3

--- src/main/java/com/jamerson/brewer/model/Estilo.java
+++ src/main/java/com/jamerson/brewer/model/Estilo.java
@@ -1,8 +1,32 @@
 package com.jamerson.brewer.model;
 
-public class Estilo {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estilo")
+public class Estilo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
 	private String nome;
+	
+	@OneToMany(mappedBy = "estilo")
+	private List<Cerveja> cervejas;
+
+	public Long getCodigo() {
+		return codigo;
+	}
 
 	public String getNome() {
 		return nome;
@@ -10,6 +34,31 @@ public class Estilo {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estilo other = (Estilo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
 	}
 
 }
